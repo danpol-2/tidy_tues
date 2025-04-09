4c4f88b8c04633d04daea54bb3e3333d73e9ef59
Daniel Polites <danielconwayjames@gmail.com>
2025-04-09 12:27:52 -0500

    clean

--- 2025 Week 14/bsky_post.R
+++
@@ -1,9 +0,0 @@
-
-# setup -------------------------------------------------------------------
-
-# API: https://christophertkenny.com/bskyr/index.html
-
-# setwd("~/repos/tidy_tues/misc")
-
-pacman::p_load(bskyr)
-
--- misc/bsky_template.R
+++ misc/bsky_template.R
@@ -22,16 +22,4 @@ pacman::p_load(bskyr)
 # bs_post(text = "I'd say so (again) (test quote)",
 #         quote = "https://bsky.app/profile/danpol2.bsky.social/post/3lm6fxvgdcd23")
 # 
-# bs_post(text = "I'd say so (again) (test combo)",
-#         reply = "https://bsky.app/profile/danpol2.bsky.social/post/3lm6fxvgdcd23",
-#         quote = "https://bsky.app/profile/danpol2.bsky.social/post/3lm6fxvgdcd23")
-# 
 # bs_repost(post = "https://bsky.app/profile/danpol2.bsky.social/post/3lm6fxvgdcd23")
-
-
-# post 2025-04-01 --------------------------------------------------------------------
-
-bs_post(text = "
-code: https://github.com/danpol-2/tidy_tues
-        ",
-        reply = "https://bsky.app/profile/danpol2.bsky.social/post/3lm6kjyioau2s")
